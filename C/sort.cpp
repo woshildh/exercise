47e590e36f02660ec81a25db748d32df1e5961d8
csldh <2948774009@qq.com>
2019-01-01 17:50:09 +0800

    Update sort.cpp

--- C/sort.cpp
+++ C/sort.cpp
@@ -57,7 +57,39 @@ void quickSort(vector<int> & nums,int start,int end)
 	quickSort(nums,start,pos-1);
 	quickSort(nums,pos+1,end);
 }
-
+void build_heap(vector<int> &nums,int low,int high)
+{
+	int last_leaf=(high-low+1)/2-1;
+	for(int i=last_leaf;i>=0;i--)
+	{
+		int left=2*i+1,right=2*i+2;
+		int pos;
+		if(right<=high)
+			pos=nums[left]>nums[right]?left:right;
+		else
+			pos=left;
+		//cout<<i<<" "<<left<<" "<<right<<" "<<pos<<endl;
+		if(nums[i] < nums[pos])
+		{
+			swap(nums[i],nums[pos]);
+		}
+	}
+}
+void heapsort(vector<int> &nums)
+{
+	int length=nums.size();
+	int k=0;
+	for(int i=length-1;i>0;i--)
+	{
+		build_heap(nums,0,i);
+		swap(nums[0],nums[i]);
+		for(int i=0;i<nums.size();i++)
+		{
+			cout<<nums[i]<<" ";
+		}
+		cout<<endl;
+	}
+}
 int main()
 {
 	int a[]={19,100,100,10000,4,1,3,2,17,99,10,6,19,30,17};
